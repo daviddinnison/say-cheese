0f8d77f1bd8dda719a9b441489215011268c2c41
Lewi Gilamichael <dr.lewi@gmail.com>
2017-08-04 13:56:06 -0400

    set actions, starting reducers

--- client/src/actions/cheese.js
+++ client/src/actions/cheese.js
@@ -2,7 +2,35 @@
 //   return(dispatch)
 // }
 
-export const fetchCheeses = () => {
-  return (dispatch) => {
+export const fetchCheeses = () => dispatch =>{
+    dispatch(fetchCheesesRequest());
   fetch('/api/cheeses')
-      .then(response => response.json())
+  .then(res => {
+    if (!res.ok) {
+      return Promise.reject(res.statusText)
+    }
+    return res.json();
+  })
+  .then(cheeses => {
+    dispatch(fetchCheesesSuccess(cheese));
+  }).catch(err => {
+    dispatch(fetchCheesesError(err))
+  })
+};
+
+export const FETCH_CHEESES_SUCCESS = 'FETCH_CHEESES_SUCCESS';
+export const fetchCheesesSuccess = cheeses => ({
+  type: FETCH_CHEESES_SUCCESS,
+  cheeses
+})
+
+export const FETCH_CHEESES_REQUEST = 'FETCH_CHEESES_REQUEST';
+export const fetchCheesesRequest = () => ({
+  type: FETCH_CHEESES_REQUEST
+})
+
+export const FETCH_CHEESES_ERROR = 'FETCH_CHEESES_ERROR';
+export const fetchCheesesError = error => ({
+  type: FETCH_CHEESES_ERROR,
+  error
+})
---
+++ client/src/reducers/cheese.js
@@ -0,0 +1,11 @@
+import {FETCH_CHEESES_ERROR, FETCH_CHEESES_SUCCESS, FETCH_CHEESES_REQUEST} from '../actions';
+
+const initialState = {
+    cheeses: [],
+    loading: false,
+    error: null
+}
+
+export const CheesesReducer = (state = initialState, action) => {
+  // if ()
+}
